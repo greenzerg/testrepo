d1e8e2543ceaaf1ee1b8127fc6d4670ac5fe730c
greenzerg <30491840+greenzerg@users.noreply.github.com>
2017-07-28 11:40:10 +0700

    Update jc_server.sh

--- jc_server.sh
+++ jc_server.sh
@@ -8,11 +8,21 @@ srvstop() {
 
 }
 
+srvinstall() {
+
+}
+
+srvhelp() {
+
+}
+
 case "$1" in 
-    start)   startme ;;
-    stop)    stopme ;;
+    start)   srvstart ;;
+    stop)    srvstop ;;
+    install)    srvinstall ::
+    help)   srvhelp;
     restart) stopme; startme ;;
-    *) echo "usage: $0 start|stop|restart" >&2
+    *) echo "usage: $0 start|stop|restart|install|help" >&2
     exit 1
     ;;
 esac
